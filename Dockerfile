f9780091bf03cdf48e64226d923d8cce8ad3fec2
Wessel Pieterse <wessel@ordercloud.co.za>
2021-01-25 09:11:34 +0200

    fix: Changed base container Changed sslVersion to TLSv1.2

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
-FROM strimzi/kafka:latest-kafka-2.4.0
+FROM ubuntu:18.04
+
+RUN apt-get update && apt-get install --no-install-recommends -y stunnel4 &&  rm -rf /var/lib/apt/lists/*
+
+ENV LOG_LEVEL debug
+ENV STUNNEL_HOME "/opt/stunnel"
+ENV ACCCEPT 0.0.0.0:2181
+
+WORKDIR "${STUNNEL_HOME}"
 
 COPY ./stunnel-scripts/ $STUNNEL_HOME
+
+CMD "./stunnel_run.sh"
--- stunnel-scripts/stunnel_config_generator.sh
+++ stunnel-scripts/stunnel_config_generator.sh
@@ -7,15 +7,15 @@ cat /etc/cluster-ca-certs/*.crt > "$CA_CERTS"
 echo "pid = /usr/local/var/run/stunnel.pid"
 echo "foreground = yes"
 echo "debug = $LOG_LEVEL"
-echo "sslVersion = all"
+echo "sslVersion = TLSv1.2"
 
 cat <<-EOF
 [zookeeper-2181]
 client = yes
 CAfile = ${CA_CERTS}
-cert = /etc/cluster-operator-certs/cluster-operator.crt
-key = /etc/cluster-operator-certs/cluster-operator.key
-accept = 0.0.0.0:2181
+cert = /etc/tls-sidecar/eo-certs/entity-operator.crt
+key = /etc/tls-sidecar/eo-certs/entity-operator.key
+accept = ${ACCEPT}
 connect = ${STRIMZI_ZOOKEEPER_CONNECT}
 delay = yes
 verify = 2
